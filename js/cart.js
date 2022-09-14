a3bd7dd45b36efa1b6eb6a4e34137b87d613b554
Cueva Tomas Santiago <cuevatomass02@gmail.com>
2022-09-14 08:20:13 -0300

    se termino la funcionalidad basica del carrito

--- js/cart.js
+++ js/cart.js
@@ -3,8 +3,20 @@
 const contenedorMates = document.getElementById("Mates")
 //contenedore del carrito
 const contenedorCarrito = document.getElementById("carrito")
+//Contador del carrito
+const contadorCarrito = document.getElementById('contadorCarrito')
+
+const precioTotal = document.getElementById('precioTotal')
 //creo el carrito
 let carrito = []
+ 
+//Funcion para no perder los datos al salir de la pagina
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        actualizarCarrito()
+    }
+})
 
 //Funciones para la creacion de las secciones de cada producto
 stockProductosMates.forEach((mate) => {
@@ -31,14 +43,37 @@ stockProductosMates.forEach((mate) => {
 //Funcion para agregar productos al carrito
 //Por ahora solo funciona con los mates, revisar para que sea global
 const agregarAlCarrito = (prodId) => {
+    //Algoritmo para no repetir elementos en al array
+    const existe = carrito.some (prod => prod.id === prodId)
+
+    if (existe){
+        const prod = carrito.map (prod => {
+            if (prod.id === prodId){
+                prod.cantidad++
+            }
+        })
+    } else {
+        //Stock solo de mates, agregar mas
     const item = stockProductosMates.find((prod) => prod.id === prodId)
     carrito.push(item)
-    actualizarCarrito()
     console.log(carrito)
+
+}
+actualizarCarrito()
+    
+}
+//El eliminar del carrito funciona en forma de cola, no borra
+//El produco exactamente seleccionado
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId)
+    const indice = carrito.indexOf(item)
+    carrito.splice(indice,1)
+    actualizarCarrito()
 }
 
 //Funcion para crear las cositas en el carrito
 const actualizarCarrito = ()=> {
+    contenedorCarrito.innerHTML = ""
     carrito.forEach((prod) => {
         const div = document.createElement('div')
         div.className = ("producto-en-carrito")
@@ -51,8 +86,12 @@ const actualizarCarrito = ()=> {
         </button>
         <label for="Cant" id="cantidad">${prod.cantidad}</label>
         <input name="Cant" type="number" value="" >
-        <p class="price">${prod.precio}</p>
+        <p class="price">${prod.precio * prod.cantidad}</p>
         `
         contenedorCarrito.appendChild(div)
+
+        localStorage.setItem('carrito', JSON.stringify(carrito))
     })
+    contadorCarrito.innerText = carrito.length
+    precioTotal.innerText = carrito.reduce((acc,prod) => acc + prod.precio * prod.cantidad,0)
 } 
